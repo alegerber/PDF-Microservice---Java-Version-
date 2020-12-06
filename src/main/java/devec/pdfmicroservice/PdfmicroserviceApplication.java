2680fffc61bf5bd2b8961db4d49bcd66b6b25bcc
Alexander Gerber <alexander.gerber@check24.de>
2020-12-06 19:43:20 +0100

    add basic

---
+++ src/main/java/devec/pdfmicroservice/PdfmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package devec.pdfmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devec/pdfmicroservice/PdfmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package devec.pdfmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
